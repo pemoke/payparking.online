aca631c698f3742eb0757143e80f3e98398fd091
Peter Marko <pemoke@gmail.com>
2016-07-31 05:26:17 +1000

    Add Leaflet

---
+++ imports/ui/App.jsx
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+// App component - represents the whole app
+export default class App extends Component {
+  componentDidMount() {
+  }
+
+  render() {
+    const position = [51.505, -0.09];
+    return (
+        <Map center={position} zoom={13}>
+          <TileLayer
+              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
+    );
+  }
+}
