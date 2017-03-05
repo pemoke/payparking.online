3a2f161a88108005ab28e53937362f4d268d5bc0
Peter Marko <pemoke@gmail.com>
2017-03-05 18:21:13 +0100

    Static functioning scaffold

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+class PayParkingDashboard extends React.Component {
+  render() {
+    return (
+        <div>
+          PayParkingDashboard
+        </div>
+    )
+  }
+}
+
+ReactDOM.render(
+    <PayParkingDashboard />,
+    document.getElementById('content')
+);
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-param-reassign */
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
