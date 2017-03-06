f08e48276acce4298c1fff452339f6e509eeeb0e
Peter Marko <pemoke@gmail.com>
2017-03-06 19:32:43 +0100

    Static version of the app

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,82 @@ class PayParkingDashboard extends React.Component {
   render() {
     return (
         <div>
-          PayParkingDashboard
+          <ParkingAllocation />
+          <Timer
+            timerIsRunning={false}
+          />
+          <TimerButton
+            timerIsRunning={false}
+          />
+          <Map />
+        </div>
+    )
+  }
+}
+
+class ParkingAllocation extends React.Component {
+  render() {
+    return (
+        <form>
+          <input type="text" />
+          <input type="text" />
+        </form>
+    )
+  }
+}
+
+class Timer extends React.Component {
+  render() {
+    if (this.props.timerIsRunning) {
+      return (
+          <TimerDisplay />
+      );
+    } else {
+      return (
+          <TimerForm />
+      );
+    }
+  }
+}
+
+class TimerForm extends React.Component {
+  render() {
+    return (
+        <form>
+          <input type="number" min="0" max="12" />
+          <input type="number" min="0" max="59" />
+          <input type="number" min="0" max="59" />
+        </form>
+    )
+  }
+}
+
+class TimerDisplay extends React.Component {
+  render() {
+    return (
+        <div>
+          HH : MM : SS
+        </div>
+    )
+  }
+}
+
+class TimerButton extends React.Component {
+  render() {
+    const timerButtonText = this.props.timerIsRunning ? 'Stop' : 'Start';
+    return (
+        <div>
+          <button>{timerButtonText}</button>
+        </div>
+    )
+  }
+}
+
+class Map extends React.Component {
+  render() {
+    return (
+        <div>
+          Map
         </div>
     )
   }
