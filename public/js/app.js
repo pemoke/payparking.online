af28f94a159d01460b7d283651098572eb558029
Peter Marko <pemoke@gmail.com>
2017-03-07 10:55:42 +0100

    Timer implementation

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,7 @@ class PayParkingDashboard extends React.Component {
     return (
         <div>
           <ParkingAllocation />
-          <Timer
-            timerIsRunning={false}
-          />
-          <TimerButton
-            timerIsRunning={false}
-          />
+          <Timer />
           <Map />
         </div>
     )
@@ -27,27 +22,134 @@ class ParkingAllocation extends React.Component {
 }
 
 class Timer extends React.Component {
+  state = {
+    timerIsRunning: false,
+    timer: {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  };
+
+  tick = () => {
+    let timerInSeconds = this.state.timer.hours * 60 * 60
+                          + this.state.timer.minutes * 60
+                          + this.state.timer.seconds;
+
+    if (timerInSeconds > 0) {
+      timerInSeconds--;
+      this.setState({
+        timer: {
+          hours: Math.floor(timerInSeconds / 60 / 60 % 60),
+          minutes: Math.floor(timerInSeconds / 60 % 60),
+          seconds: Math.floor(timerInSeconds % 60)
+        }
+      });
+    } else {
+      this.handleTimerStop();
+    }
+  };
+
+  handleTimerStart = () => {
+    this.setState({ timerIsRunning: true });
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  handleTimerStop = () => {
+    this.setState({ timerIsRunning: false });
+    clearInterval(this.interval);
+  };
+
+  handleTimerChange = (e) => {
+    if (e.target.name === 'hours') {
+      this.setState({
+        timer: {
+          hours: parseInt(e.target.value),
+          minutes: this.state.timer.minutes,
+          seconds: this.state.timer.seconds
+        }
+      })
+    } else if (e.target.name === 'minutes') {
+      this.setState({
+        timer: {
+          hours: this.state.timer.hours,
+          minutes: parseInt(e.target.value),
+          seconds: this.state.timer.seconds
+        }
+      })
+    } else if (e.target.name === 'seconds') {
+      this.setState({
+        timer: {
+          hours: this.state.timer.hours,
+          minutes: this.state.timer.minutes,
+          seconds: parseInt(e.target.value)
+        }
+      })
+    }
+  };
+
   render() {
-    if (this.props.timerIsRunning) {
+    let timerClock = {
+      HH: ('0' + this.state.timer.hours).slice(-2),
+      MM: ('0' + this.state.timer.minutes).slice(-2),
+      SS: ('0' + this.state.timer.seconds).slice(-2)
+    };
+
+    if (this.state.timerIsRunning) {
       return (
-          <TimerDisplay />
+          <TimerDisplay
+            timerIsRunning={this.state.timerIsRunning}
+            handleTimerStop={this.handleTimerStop}
+            timerClock={timerClock}
+          />
       );
     } else {
       return (
-          <TimerForm />
+          <TimerForm
+            timerIsRunning={this.state.timerIsRunning}
+            handleTimerStart={this.handleTimerStart}
+            handleTimerChange={this.handleTimerChange}
+            timerClock={timerClock}
+          />
       );
     }
+
   }
 }
 
 class TimerForm extends React.Component {
   render() {
     return (
-        <form>
-          <input type="number" min="0" max="12" />
-          <input type="number" min="0" max="59" />
-          <input type="number" min="0" max="59" />
-        </form>
+        <div>
+          <input
+              type="number"
+              name="hours"
+              min="0"
+              max="12"
+              defaultValue={this.props.timerClock.HH}
+              onChange={this.props.handleTimerChange}
+          />
+          <input
+              type="number"
+              name="minutes"
+              min="0"
+              max="59"
+              defaultValue={this.props.timerClock.MM}
+              onChange={this.props.handleTimerChange}
+          />
+          <input
+              type="number"
+              name="seconds"
+              min="0"
+              max="59"
+              defaultValue={this.props.timerClock.SS}
+              onChange={this.props.handleTimerChange}
+          />
+          <TimerButton
+              timerIsRunning={this.props.timerIsRunning}
+              onTimerStart={this.props.handleTimerStart}
+          />
+        </div>
     )
   }
 }
@@ -56,7 +158,15 @@ class TimerDisplay extends React.Component {
   render() {
     return (
         <div>
-          HH : MM : SS
+          {this.props.timerClock.HH}
+          :
+          {this.props.timerClock.MM}
+          :
+          {this.props.timerClock.SS}
+          <TimerButton
+            timerIsRunning={this.props.timerIsRunning}
+            onTimerStop={this.props.handleTimerStop}
+          />
         </div>
     )
   }
@@ -64,12 +174,27 @@ class TimerDisplay extends React.Component {
 
 class TimerButton extends React.Component {
   render() {
-    const timerButtonText = this.props.timerIsRunning ? 'Stop' : 'Start';
-    return (
-        <div>
-          <button>{timerButtonText}</button>
-        </div>
-    )
+    if (this.props.timerIsRunning) {
+      return (
+          <div>
+            <button
+                onClick={this.props.onTimerStop}
+            >
+              Stop
+            </button>
+          </div>
+      )
+    } else {
+      return (
+          <div>
+            <button
+                onClick={this.props.onTimerStart}
+            >
+              Start
+            </button>
+          </div>
+      )
+    }
   }
 }
 
