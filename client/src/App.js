114f980dcff7179914a6182cf21c5cc935914d7f
Peter Marko <pemoke@gmail.com>
2017-03-19 13:24:44 +0100

    AppBar with material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
 import Parking from './components/Parking';
-import logo from './logo.svg';
+
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+
+import {Card} from 'material-ui/Card';
+import IconButton from 'material-ui/IconButton';
+import IconMyLocation from 'material-ui/svg-icons/maps/my-location';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-class App extends React.Component {
+const styles = {
+  title: {
+    cursor: 'pointer',
+  },
+};
+
+class Location extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>PayParking.online</h2>
-          </div>
-          <Parking />
+        <div>
+          <IconButton>
+            <IconMyLocation />
+          </IconButton>
         </div>
-      </MuiThemeProvider>
+    )
+  }
+}
+
+class App extends React.Component {
+  handleTouchTap = () => {
+    alert('onTouchTap triggered on the title component');
+  };
+
+  render() {
+    return (
+        <MuiThemeProvider>
+          <div className="App">
+            <AppBar
+                title={<span style={styles.title}>PayParking.online</span>}
+                onTitleTouchTap={this.handleTouchTap}
+                iconElementRight={<Location />}
+            />
+            <Card>
+                <Parking />
+            </Card>
+          </div>
+        </MuiThemeProvider>
     );
   }
 }
